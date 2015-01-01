16388d9a1822721af9e5ab8d29945c54487ec10e
Jeffrey Meyerson <meff.jeyerson@gmail.com>
2014-12-31 17:41:07 -0800

    added chart

---
+++ rails_app/app/assets/javascripts/samplechart.js
@@ -0,0 +1,16 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+console.log("in samplechart");
