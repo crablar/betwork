b7fea22aab06f5366fe8149e3ab9813551890476
Jeffrey Meyerson <meff.jeyerson@gmail.com>
2015-01-20 13:30:59 -0800

    one step forward two steps back

--- rails_app/app/assets/javascripts/samplechart.js
+++ rails_app/app/assets/javascripts/samplechart.js
@@ -5,22 +5,28 @@ $.getJSON("/charts.json", function(json) {
     console.log(arr);
     for (var i = 0; i < arr.length; i++) {
         var chart = arr[i];
-        console.log(chart);
         var events = chart["events"];
-        console.log(events);
+        var values = [];
+        var timestamps = [];
         for(var j = 1; j < events.length; j++){
-            events[j]["value"] -= events[j - 1]["value"];
+            console.log(events[j]);
+            //values.push(events[j]["value"] - events[j - 1]["value"]);
+            values.push(1);
+            timestamps.push(events[j]["timestamp"]);
         }
-        events[0] = 0;
         chart["events"] = events;
-        console.log(chart);
+        console.log("YOO");
+        console.log(values);
+        console.log(timestamps);
+        console.log(chart["chartSubject"]["subjectName"]);
+        console.log(chart["chartSubject"]["valueType"]);
         $(function () {
             new Highcharts.Chart({
                 chart: {
-                    renderTo: chart["subjectName"] + 'Chart'
+                    renderTo: chart["chartSubject"]["subjectName"] + 'Chart'
                 },
                 title: {
-                    text: chart.subjectName,
+                    text: chart["chartSubject"]["subjectName"],
                     x: -20 //center
                 },
                 subtitle: {
@@ -28,11 +34,11 @@ $.getJSON("/charts.json", function(json) {
                     x: -20
                 },
                 xAxis: {
-                    categories: chart.timestamps,
+                    categories: timestamps,
                 },
                 yAxis: {
                     title: {
-                        text: chart.valueType
+                        text: chart["chartSubject"]["valueType"]
                     },
                     plotLines: [{
                         value: 0,
@@ -50,8 +56,8 @@ $.getJSON("/charts.json", function(json) {
                     borderWidth: 0
                 },
                 series: [{
-                    name: chart.subjectName,
-                    data: chart.values
+                    name: chart["chartSubject"]["subjectName"],
+                    data: values
                 }]
             });
         });
